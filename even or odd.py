53445eb7ec77af73bedeffc52aaee9e6feb3893b
rozwar188 <maheshbhandari188@gmail.com>
2016-12-01 22:57:44 -0800

    Add files via upload
    foss practical 2016

---
+++ even or odd.py
@@ -0,0 +1,6 @@
+a=int(input('enter the first number'))
+b=int(input('enter the second number'))
+if(a%2==0):
+    print('the no is even')
+else:
+    print('the no is odd')
---
+++ foss practical.py
@@ -0,0 +1,5 @@
+remove()
+x=['to','be','or','not','to','and']
+x.remove('be')
+print(x)
+      
